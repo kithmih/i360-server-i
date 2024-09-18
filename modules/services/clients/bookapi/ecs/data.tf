c74f5a6c3d407204d4683b74cd722c0192a1ace0
kithmih <kithmih@i360.lk>
2024-09-18 17:03:40 +0530

    correcting repo name

--- modules/services/clients/bookapi/ecs/data.tf
+++ modules/services/clients/bookapi/ecs/data.tf
@@ -1,6 +1,6 @@
 data "aws_ecr_repository" "ecr" {
   for_each = aws_cloudwatch_log_group.logs
 
-  name = "${var.owner}-${var.project}-umbartha-${var.client.services[each.value.tags.key].name}"
+  name = "${var.owner}-${var.project}-bookapi-${var.client.services[each.value.tags.key].name}"
 }
 
